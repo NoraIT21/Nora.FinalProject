af28098bb12b65205391aa468ad36810dfd03fc2
Nora <159137799+NoraIT21@users.noreply.github.com>
2024-05-14 13:47:51 +0300

    add encoded
    I add the encoded method

--- Steo.py
+++ Steo.py
@@ -57,3 +57,14 @@ from stegapy import decode_image
 
 decoded = decode_image('secret-image.png')
 print(decoded)
+
+
+from stegapy import create_image
+
+message = '''Steganography is the practice of concealing a file, 
+message, image, or video within another file, message, image,
+or video. The word steganography combines the Greek words 
+steganos, meaning "covered or concealed", and graphe meaning
+"writing".'''
+create_image(message, 'original-image.png', 'secret-image.png')
+
